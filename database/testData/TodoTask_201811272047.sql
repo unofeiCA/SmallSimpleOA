de89a85aa72d308a06e797eafef799ae283a724e
yufei <unofei@gmail.com>
2018-11-27 20:48:10 -0600

    database ddl and test data updated;

---
+++ database/testData/TodoTask_201811272047.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO SmallSimpleOA.dbo.TodoTask (Valid,Status,Title,Content,CreateTime,ModifyTime,DeadLine,UzerID) VALUES 
+(1,2,'long away todo','ddd','2018-11-25 20:50:56.967',NULL,'2019-07-18 20:49:00.000',2)
+,(1,2,'approching todo edit from todo list','approching todo edit from todo list','2018-11-25 20:55:09.127','2018-11-27 20:14:25.307','2019-03-07 20:54:00.000',2)
+,(0,4,'due soon todo','due soon todo','2018-11-25 21:08:40.977','2018-11-27 20:18:53.990','2018-12-06 21:08:00.000',2)
+,(1,1,'due todo','due todo','2018-11-25 21:09:12.467',NULL,'2018-11-25 21:10:00.000',2)
+,(1,1,'done todo','done todo','2018-11-25 21:09:27.363','2018-11-25 21:30:38.513','2018-12-07 21:09:00.000',2)
+,(0,4,'need delete todo','need delete todo','2018-11-25 21:09:47.993','2018-11-25 21:46:13.750','2018-12-08 21:09:00.000',2)
+,(1,4,'superlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todo','superlong todosuperlong todosuperlong todo','2018-11-25 21:22:34.423',NULL,'2018-12-08 21:22:00.000',2)
+,(1,1,'edit from home','edit from home','2018-11-25 21:47:03.773','2018-11-27 20:13:50.347','2018-12-08 21:48:00.000',2)
+,(0,1,'test from home','test from home','2018-11-27 20:13:20.940','2018-11-27 20:18:50.680','2018-12-07 20:13:00.000',2)
+,(0,4,'test from todo list','test from todo list','2018-11-27 20:13:35.923','2018-11-27 20:18:38.277','2018-12-08 20:13:00.000',2)
+;

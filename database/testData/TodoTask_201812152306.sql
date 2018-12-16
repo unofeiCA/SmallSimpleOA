71eab0fa009f9807667c834b5de29fddceec40c5
yufei <unofei@gmail.com>
2018-12-15 23:11:08 -0600

    test data updated;

--- database/testData/TodoTask_201812152306.sql
+++ database/testData/TodoTask_201812152306.sql
@@ -5,16 +5,20 @@
 ,(1,1,'due todo','due todo','2018-11-25 21:09:12.467',NULL,'2018-11-25 21:10:00.000',2)
 ,(1,1,'done todo','done todo','2018-11-25 21:09:27.363','2018-11-25 21:30:38.513','2018-12-07 21:09:00.000',2)
 ,(0,4,'need delete todo','need delete todo','2018-11-25 21:09:47.993','2018-11-25 21:46:13.750','2018-12-08 21:09:00.000',2)
-,(1,5,'superlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todo','superlong todosuperlong todosuperlong todo','2018-11-25 21:22:34.423',NULL,'2018-12-08 21:22:00.000',2)
+,(0,5,'superlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todosuperlong todo','superlong todosuperlong todosuperlong todo','2018-11-25 21:22:34.423','2018-12-15 23:00:07.270','2018-12-08 21:22:00.000',2)
 ,(1,1,'edit from home','edit from home','2018-11-25 21:47:03.773','2018-11-27 20:13:50.347','2018-12-08 21:48:00.000',2)
 ,(0,1,'test from home','test from home','2018-11-27 20:13:20.940','2018-11-27 20:18:50.680','2018-12-07 20:13:00.000',2)
 ,(0,4,'test from todo list','test from todo list','2018-11-27 20:13:35.923','2018-11-27 20:18:38.277','2018-12-08 20:13:00.000',2)
 ;
 INSERT INTO SmallSimpleOA.dbo.TodoTask (Valid,Status,Title,Content,CreateTime,ModifyTime,DeadLine,UzerID) VALUES 
 (1,1,'due todo','drde','2018-11-30 21:30:19.390',NULL,'2018-12-01 21:30:00.000',2)
-,(1,4,'approching todo',NULL,'2018-11-30 21:48:02.187',NULL,'2018-12-10 21:47:00.000',2)
+,(0,5,'approching todo',NULL,'2018-11-30 21:48:02.187','2018-12-15 23:00:12.793','2018-12-10 21:47:00.000',2)
 ,(1,5,'done todo',NULL,'2018-11-30 21:48:20.353',NULL,'2018-12-05 21:48:00.000',2)
-,(1,5,'due soon todo',NULL,'2018-11-30 21:50:03.620',NULL,'2018-11-30 21:51:00.000',2)
+,(0,5,'due soon todo',NULL,'2018-11-30 21:50:03.620','2018-12-15 23:00:21.747','2018-11-30 21:51:00.000',2)
 ,(1,4,'due todo',NULL,'2018-12-01 22:03:10.750',NULL,'2019-01-05 22:03:00.000',2)
 ,(1,4,'ccccc','cccc','2018-12-06 22:49:19.913',NULL,'2019-01-04 22:49:00.000',1002)
+,(1,2,'far away todo task','a far away todo task','2018-12-15 22:56:56.597','2018-12-15 22:59:36.447','2019-05-08 22:56:00.000',1003)
+,(1,3,'approching todo','an approching todo','2018-12-15 22:57:28.987',NULL,'2019-01-31 22:57:00.000',1003)
+,(1,4,'due soon todo','a due soon todo','2018-12-15 22:57:56.670',NULL,'2019-01-03 22:57:00.000',1003)
+,(1,1,'done todo','a done todo','2018-12-15 22:58:21.183',NULL,'2019-01-04 22:58:00.000',1003)
 ;
